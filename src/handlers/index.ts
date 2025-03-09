847f215590ec726548104ce4a5b1377555567e10
janr0599 <javiernr0599@gmail.com>
2025-03-09 17:21:06 -0400

    Update src/handlers/index.ts to return just the token as response

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -76,10 +76,7 @@ export const login = async (req: Request, res: Response) => {
         // Generate JWT
         const token = generateJWT({ id: user._id });
 
-        res.status(200).json({
-            message: "Usuario autenticado corectamente",
-            token,
-        });
+        res.status(200).json({ token });
     } catch (error) {
         res.status(500).json({ error: "Ha ocurrido un error" });
     }
